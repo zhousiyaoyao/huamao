a4281e53ae6780be7a9b61ed168ce5eb80b45ac8
ZEUS ZHOU <109420934@qq.com>
2025-01-07 23:24:23 +0800

    feat: mvp version

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,18 +1,35 @@
 Page({
   data: {
-    images: [
-      { url: './images/swiper1.jpg' },
-      { url: './images/swiper2.jpg' },
-      { url: './images/swiper3.jpg' },
-      { url: './images/swiper4.jpg' },
-    ],
+    images: [],
+    imageUrls: [],
     // statusBarHeight: 0,
   },
   onLoad (options) {
-    // const systemInfo = wx.getSystemInfoSync();
-    // this.setData({
-    //   statusBarHeight: systemInfo.statusBarHeight,
-    // });
+    wx.cloud.getTempFileURL({
+      fileList: [
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/swiper1.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/swiper2.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/swiper3.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/swiper4.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/front.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/ele.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/new1.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/old1.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/family.jpg', maxAge: 3600 },
+        { fileID: 'cloud://huamao-1go6njsdbc59a621.6875-huamao-1go6njsdbc59a621-1329850573/image/images/breakfast1.jpg', maxAge: 3600 },
+      ],
+      success: (res) => {
+        this.setData({
+          imageUrls: res.fileList.slice(4).map((item) =>item.tempFileURL),
+          images: res.fileList.slice(0,4).map((item) => {
+            return {
+              url: item.tempFileURL
+            }
+          }),
+        });
+      },
+      fail: (err) => console.error('获取临时链接失败：', err),
+    });
   },
   onPullDownRefresh () {
   },
